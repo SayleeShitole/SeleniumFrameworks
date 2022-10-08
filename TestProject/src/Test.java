ced664d01290092aa2d9be4ffa12e33744783e24
SayleeShitole <sayleeshitole1910@gmail.com>
2022-10-08 16:58:37 +0530

    First commit

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Name Saylee");
+	
 		
-		System.out.println("Saylee");
+		System.out.println("Test ");
 	}
 }
